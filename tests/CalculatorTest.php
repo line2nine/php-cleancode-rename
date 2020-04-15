89062a2a1d131477a1282d97bafe393703994a16
line2nine <hailinh203@gmail.com>
2020-04-15 08:13:18 +0700

    Add files

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,23 @@
+<?php
+require __DIR__ . "/../src/Calculator.php";
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    public function testCalculateAdd()
+    {
+
+        $firstOperand = 1;
+        $secondOperand = 1;
+        $operator = ADDITION;
+
+        $expected = 2;
+
+        $calculator = new Calculator();
+        $result = $calculator->calculate($firstOperand, $secondOperand, $operator);
+        $this->assertEquals($expected, $result);
+    }
+
+
+
+}
